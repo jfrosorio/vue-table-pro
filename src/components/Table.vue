f2021ee5cd1c455b61767eb6fb09c3d7e1c0beb9
chicop <olachico@icloud.com>
2018-05-27 19:25:19 +0100

    added a test suite

---
+++ src/components/Table.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="table">
+</div>
+</template>
+
+<script>
+export default {
+	name: 'Table',
+	data() {
+		return {
+			count: 0
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
