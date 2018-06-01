ccb27db6dd1c39ef918dbd70c882f414de8a79c6
jfrosorio <jfrosorio@gmail.com>
2018-06-01 12:38:07 +0100

    Search component initial commit

--- src/components/Vuetablepro.vue
+++ src/components/Vuetablepro.vue
@@ -1,40 +1,52 @@
 <template>
-    <table>
-        <thead>
-            <tr>
-                <th>Col 1</th>
-                <th>Col 2</th>
-                <th>Col 3</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Data 1</td>
-                <td>Data 2</td>
-                <td>Data 3</td>
-            </tr>
-            <tr>
-                <td>Data 1</td>
-                <td>Data 2</td>
-                <td>Data 3</td>
-            </tr>
-            <tr>
-                <td>Data 1</td>
-                <td>Data 2</td>
-                <td>Data 3</td>
-            </tr>
-        </tbody>
-    </table>
+    <div>
+        <VuetableproSearch :rows="rows" />
+
+        <table style="width: 500px">
+            <thead>
+                <tr>
+                    <th v-for="column in columns" :style="{width: column.width}">{{ column.name }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="row in rows">
+                    <td v-for="data in row">{{ data }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
+    import VuetableproSearch from './VuetableproSearch';
+
     export default {
         name: 'Vuetablepro',
         data() {
             return {
-                count: 0
+                columns: [
+                    {
+                        name: 'Id',
+                        width: '20%'
+                    },
+                    {
+                        name: 'Name',
+                        width: '20%'
+                    },
+                    {
+                        name: 'Job'
+                    }
+                ],
+                rows: [
+                    ['1', 'John Smith', 'Bank robber'],
+                    ['5', 'Samantha Cole', 'Nurse'],
+                    ['30', 'Robert van Zant', 'Investor'],
+                    ['125', 'Trisha Portman', 'Waitress'],
+                    ['255', 'Paul Newman', 'Detective'],
+                ]
             }
-        }
+        },
+        components: { VuetableproSearch }
     }
 </script>
 
