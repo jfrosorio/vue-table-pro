17c3d9d1fbc370dfaaadeb2b711da79c97420df8
chicop <olachico@icloud.com>
2018-08-21 00:17:33 +0100

    new architecture configuration

---
+++ src/components/Table.vue
@@ -0,0 +1,70 @@
+<template>
+  <table>
+    <caption v-if="caption">{{ caption }}</caption>
+    <thead>
+      <tr>
+        <th v-for="header in headers" @click="headingClicked">{{ header }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="entry in tableData">
+        <td v-for="property in entry">{{ property }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: 'Vue_table_pro',
+    props: {
+      caption: 'hello',
+    },
+    data () {
+      return {
+        tableData: [
+          {
+            name: 'Lion',
+            age: 5,
+            weight: 20,
+          },
+          {
+            name: 'Lion',
+            age: 3,
+            weight: 20,
+          },
+          {
+            name: 'Lion',
+            age: 2,
+            weight: 20,
+          },
+          {
+            name: 'Lion',
+            age: 7,
+            weight: 20,
+          },
+        ],
+        headers: [],
+      }
+    },
+    methods: {
+      headingClicked () {
+      },
+      sort () {
+      },
+    },
+    mounted () {
+      this.tableData.forEach((entry) => {
+        Object.keys(entry).forEach((key) => {
+          if (this.headers.indexOf(key) === -1) {
+            this.headers.push(key)
+          }
+        })
+      })
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
