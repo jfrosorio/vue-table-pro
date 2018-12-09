619a68fc6497a5c2e7ed69eeb0cb09f01b7b121b
chicop <olachico@icloud.com>
2018-12-09 12:50:34 +0000

    added beforeEach function to tests and vue instance test

--- tests/unit/Table.spec.js
+++ tests/unit/Table.spec.js
@@ -4,29 +4,28 @@ import Table from '@/components/Table.vue'
 import rows from './mock-data/rows.mock'
 
 describe('Table', () => {
+  let tableProps
+
+  beforeEach(() => {
+    tableProps = {
+      rows,
+      caption: "Cars List"
+    }
+  })
+  
   it('does not exist without rows', () => {
     const wrapper = shallowMount(Table)
     expect(wrapper.html()).toBeUndefined()
   })
 
   it('renders the root element', () => {
-    const wrapper = shallowMount(Table, {
-      propsData: {
-        rows,
-        caption: 'Cars List'
-      }
-    })
+    const wrapper = shallowMount(Table, { propsData: tableProps })
 
     expect(wrapper.html()).toContain('<div class="vuetable">')
   })
 
   it('has a caption', () => {
-    const wrapper = shallowMount(Table, {
-      propsData: {
-        rows,
-        caption: 'Cars List'
-      }
-    })
+    const wrapper = shallowMount(Table, { propsData: tableProps })
 
     // Test if the caption exists
     expect(wrapper.contains('caption')).toBe(true)
