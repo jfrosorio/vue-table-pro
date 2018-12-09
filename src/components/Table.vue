287161188800fd94bee8807d74b9e55988e91551
Miguel Teixeira <teixeira.silva.miguel@gmail.com>
2018-12-09 17:02:42 +0000

    Fix eslint spacing errors

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -197,15 +197,15 @@ export default {
         let customColumns = {}
         for (const column in this.columns) {
           if (this.columns.hasOwnProperty(column) && this.expandable.withColumns.includes(column)) {
-            const element = {[column]: this.columns[column]}
-            customColumns = {...customColumns, ...element}
+            const element = { [column]: this.columns[column] }
+            customColumns = { ...customColumns, ...element }
           }
         }
         this.expandableFields = customColumns
       }
 
       if (expandAttachedFields) {
-        this.expandableFields = {...this.expandableFields, ...this.expandable.attachFields}
+        this.expandableFields = { ...this.expandableFields, ...this.expandable.attachFields }
       }
     },
     _toggleExpandable (index) {
